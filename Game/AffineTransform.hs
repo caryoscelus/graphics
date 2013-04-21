d82a793617aa987d76df6b75c949c51f1d99d154
Jake McArthur <Jake.McArthur@gmail.com>
2013-04-21 18:12:19 -0400

    begin work on affine transformations. this stuff could use a lot more polish

---
+++ Game/AffineTransform.hs
@@ -0,0 +1,89 @@
+module Game.AffineTransform where
+
+import Control.Applicative
+import Data.Monoid
+import Linear.Epsilon
+import Linear.Matrix
+import Linear.V2
+import Linear.V3
+
+-- CR jmcarthur: We could use a more efficient representation. The
+-- bottom row can probably be left off. It would just require some
+-- more manual work to implement all the operations. Eventually, I
+-- want to actually do this.
+
+-- CR jmcarthur: Moar instances.
+newtype AffineTransform a = AffineTransform (V3 (V3 a)) deriving Show
+
+instance Num a => Monoid (AffineTransform a) where
+  {-# INLINE mempty #-}
+  mempty = AffineTransform eye3
+  {-# INLINE mappend #-}
+  AffineTransform a `mappend` AffineTransform b = AffineTransform $ a !*! b
+
+bottomRow :: Num a => V3 a
+{-# INLINE bottomRow #-}
+bottomRow = V3 0 0 1
+
+translate :: Num a => V2 a -> AffineTransform a
+{-# INLINE translate #-}
+translate (V2 x y) =
+  AffineTransform (V3 (V3 1 0 x)
+                      (V3 0 1 y)
+                      bottomRow)
+
+rotate :: Floating a => a -> AffineTransform a
+{-# INLINE rotate #-}
+rotate r =
+  AffineTransform (V3 (V3 cosr (-sinr) 0)
+                      (V3 sinr   cosr  0)
+                      bottomRow)
+  where cosr = cos r
+        sinr = sin r
+
+scale :: Num a => V2 a -> AffineTransform a
+{-# INLINE scale #-}
+scale (V2 x y) =
+  AffineTransform (V3 (V3 x 0 0)
+                      (V3 0 y 0)
+                      bottomRow)
+
+shear :: Num a => V2 a -> AffineTransform a
+{-# INLINE shear #-}
+shear (V2 x y) =
+  AffineTransform (V3 (V3 1 x 0)
+                      (V3 y 1 0)
+                      bottomRow)
+
+reflect :: Num a => V2 a -> AffineTransform a
+{-# INLINE reflect #-}
+reflect (V2 x y) =
+  AffineTransform (V3 (V3 (x2-y2) twoXY   0)
+                      (V3 twoXY   (y2-x2) 0)
+                      bottomRow)
+  where twoXY = 2 * x * y
+        x2    = x * x
+        y2    = y * y
+
+invert :: (Floating a, Epsilon a) => AffineTransform a -> Maybe (AffineTransform a)
+{-# INLINE invert #-}
+invert (AffineTransform a) = AffineTransform <$> inv33 a
+
+apply :: Num a => AffineTransform a -> V2 a -> V2 a
+{-# INLINE apply #-}
+apply (AffineTransform a) (V2 x y) =
+  let V3 x' y' _ = a !* V3 x y 1
+  in V2 x' y'
+
+-- | [[applyFourCorners01 a]] = (apply a (0,0), apply a (0,1), apply a (1,0), apply a (1,1))
+applyFourCorners01 :: (Num a, Eq a) => AffineTransform a -> (V2 a, V2 a, V2 a, V2 a)
+{-# INLINE applyFourCorners01 #-}
+applyFourCorners01 (AffineTransform (V3 (V3 a11 a12 a13)
+                                        (V3 a21 a22 a23)
+                                        (V3 0   0   1))) =
+  (zz, zzzo, zz + oz, zzzo + oz)
+  where zz = V2 a13 a23
+        zo = V2 a12 a22
+        oz = V2 a11 a21
+        zzzo = zz + zo
+applyFourCorners01 _ = error "bottom row is not 0 0 1"
--- Game/Graphics.hs
+++ Game/Graphics.hs
@@ -1,2 +1,79 @@
+{-# LANGUAGE RankNTypes #-}
 module Game.Graphics where
 
+import Control.Applicative
+import Control.Monad
+import Data.Monoid
+import Linear.V2
+import Text.Show
+
+import Game.AffineTransform as Transform
+
+newtype Space c a =
+  Space { runSpace :: forall r. Monoid r => (AffineTransform c -> a -> r) -> r }
+
+instance (Show a, Show c) => Show (Space c a) where
+  showsPrec p s =
+    showParen (p > 10) $
+    showString "asum " .
+    showListWith id (runSpace s showOne)
+    where showOne t x =
+            pure $
+            showsPrec 4 x .
+            showString " <$ " .
+            showString "transform " .
+            showsPrec 11 t
+
+instance Functor (Space c) where
+  {-# INLINE fmap #-}
+  fmap f (Space s) = Space $ \k -> s $ \t -> k t . f
+
+instance Num c => Applicative (Space c) where
+  {-# INLINE pure #-}
+  pure x = Space $ \k -> k mempty x
+  {-# INLINE (<*>) #-}
+  Space sf <*> Space sx =
+    Space $ \k -> sf $ \t f -> sx $ \t' -> k (t <> t') . f
+
+instance Num c => Alternative (Space c) where
+  {-# INLINE empty #-}
+  empty = Space mempty
+  {-# INLINE (<|>) #-}
+  Space f <|> Space g = Space $ f <> g
+
+instance Num c => Monad (Space c) where
+  {-# INLINE return #-}
+  return = pure
+  {-# INLINE (>>=) #-}
+  Space s >>= f = Space $ \k -> s $ \t x -> case f x of
+    Space s' -> s' $ \t' -> k $ t <> t'
+
+instance Num c => MonadPlus (Space c) where
+  {-# INLINE mzero #-}
+  mzero = empty
+  {-# INLINE mplus #-}
+  mplus = (<|>)
+
+transform :: AffineTransform c -> Space c ()
+{-# INLINE transform #-}
+transform t = Space $ \k -> k t ()
+
+translate :: Num c => V2 c -> Space c ()
+{-# INLINE translate #-}
+translate = transform . Transform.translate
+
+rotate :: Floating c => c -> Space c ()
+{-# INLINE rotate #-}
+rotate = transform . Transform.rotate
+
+scale :: Num c => V2 c -> Space c ()
+{-# INLINE scale #-}
+scale = transform . Transform.scale
+
+shear :: Num c => V2 c -> Space c ()
+{-# INLINE shear #-}
+shear = transform . Transform.shear
+
+reflect :: Num c => V2 c -> Space c ()
+{-# INLINE reflect #-}
+reflect = transform . Transform.reflect
