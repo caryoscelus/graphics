4b08977ff73a9908a86944c4ad7b1f8696b8526a
Jake McArthur <Jake.McArthur@gmail.com>
2013-05-11 17:42:53 -0400

    make Space a monad transformer

--- Game/Graphics.hs
+++ Game/Graphics.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ImplicitParams #-}
 module Game.Graphics where
 
 import Control.Applicative
 import Control.Monad
+import Data.Functor.Identity
 import Data.Monoid
 import Linear.V2
 import Text.Show
@@ -12,8 +15,14 @@ import Game.AffineTransform as Transform
 -- CR jmcarthur: It is not immediately clear whether this representation is
 -- actually a win or a loss in terms of efficiency, especially in terms of
 -- duplication of work.
-newtype Space c a =
-  Space { runSpace :: forall r. Monoid r => (AffineTransform c -> a -> r) -> r }
+newtype SpaceT c m a =
+  SpaceT { runSpaceT :: forall r. Monoid r => (AffineTransform c -> a -> m r) -> m r }
+
+type Space c a = SpaceT c Identity a
+
+runSpace :: Monoid r => Space c a -> (AffineTransform c -> a -> r) -> r
+{-# INLINE runSpace #-}
+runSpace (SpaceT s) f = runIdentity . s $ (fmap.fmap) Identity f
 
 instance (Show a, Show c) => Show (Space c a) where
   showsPrec p s =
@@ -27,56 +36,56 @@ instance (Show a, Show c) => Show (Space c a) where
             showString "transform " .
             showsPrec 11 t
 
-instance Functor (Space c) where
+instance Functor (SpaceT c m) where
   {-# INLINE fmap #-}
-  fmap f (Space s) = Space $ \k -> s $ \t -> k t . f
+  fmap f (SpaceT s) = SpaceT $ \k -> s $ \t -> k t . f
 
-instance Num c => Applicative (Space c) where
+instance Num c => Applicative (SpaceT c m) where
   {-# INLINE pure #-}
-  pure x = Space $ \k -> k mempty x
+  pure x = SpaceT $ \k -> k mempty x
   {-# INLINE (<*>) #-}
-  Space sf <*> Space sx =
-    Space $ \k -> sf $ \t f -> sx $ \t' -> k (t <> t') . f
+  SpaceT sf <*> SpaceT sx =
+    SpaceT $ \k -> sf $ \t f -> sx $ \t' -> k (t <> t') . f
 
-instance Num c => Alternative (Space c) where
+instance (Applicative m, Num c) => Alternative (SpaceT c m) where
   {-# INLINE empty #-}
-  empty = Space mempty
+  empty = SpaceT $ (pure.pure) mempty
   {-# INLINE (<|>) #-}
-  Space f <|> Space g = Space $ f <> g
+  SpaceT f <|> SpaceT g = SpaceT $ (liftA2.liftA2) (<>) f g
 
-instance Num c => Monad (Space c) where
+instance Num c => Monad (SpaceT c m) where
   {-# INLINE return #-}
   return = pure
   {-# INLINE (>>=) #-}
-  Space s >>= f = Space $ \k -> s $ \t x -> case f x of
-    Space s' -> s' $ \t' -> k $ t <> t'
+  SpaceT s >>= f = SpaceT $ \k -> s $ \t x -> case f x of
+    SpaceT s' -> s' $ \t' -> k $ t <> t'
 
-instance Num c => MonadPlus (Space c) where
+instance (Monad m, Num c) => MonadPlus (SpaceT c m) where
   {-# INLINE mzero #-}
-  mzero = empty
+  mzero = SpaceT $ (return.return) mempty
   {-# INLINE mplus #-}
-  mplus = (<|>)
+  SpaceT f `mplus` SpaceT g = SpaceT $ (liftM2.liftM2) (<>) f g
 
-transform :: AffineTransform c -> Space c ()
+transform :: AffineTransform c -> SpaceT c m ()
 {-# INLINE transform #-}
-transform t = Space $ \k -> k t ()
+transform t = SpaceT $ \k -> k t ()
 
-translate :: Num c => V2 c -> Space c ()
+translate :: Num c => V2 c -> SpaceT c m ()
 {-# INLINE translate #-}
 translate = transform . Transform.translate
 
-rotate :: Floating c => c -> Space c ()
+rotate :: Floating c => c -> SpaceT c m ()
 {-# INLINE rotate #-}
 rotate = transform . Transform.rotate
 
-scale :: Num c => V2 c -> Space c ()
+scale :: Num c => V2 c -> SpaceT c m ()
 {-# INLINE scale #-}
 scale = transform . Transform.scale
 
-shear :: Num c => V2 c -> Space c ()
+shear :: Num c => V2 c -> SpaceT c m ()
 {-# INLINE shear #-}
 shear = transform . Transform.shear
 
-reflect :: Num c => V2 c -> Space c ()
+reflect :: Num c => V2 c -> SpaceT c m ()
 {-# INLINE reflect #-}
 reflect = transform . Transform.reflect
