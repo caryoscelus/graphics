aef4418b4520cb9b2c08fe7795464dfe8f09d2de
Jake McArthur <Jake.McArthur@gmail.com>
2013-06-05 06:28:35 -0400

    some ugly work in progress on buffer streaming

--- Game/Stream.hs
+++ Game/Stream.hs
@@ -5,6 +5,8 @@
 module Game.Stream where
 
 import Control.Applicative
+import Control.Monad
+import Data.Bits
 import Data.List
 import Foreign.Ptr
 import Foreign.Storable
@@ -13,6 +15,7 @@ import Game.Sprite
 import Graphics.Rendering.OpenGL.Raw.Core31
 import Linear.V2
 
+-- Attributes for a single vertex
 data Attribs =
   Attribs { xPos :: !GLfloat
           , yPos :: !GLfloat
@@ -35,6 +38,7 @@ instance Storable Attribs where
     pokeElemOff ptr (off+2) xTex
     pokeElemOff ptr (off+3) yTex
 
+-- Attributes for a triangle (the corners must be in clockwise order)
 data TriangleAttribs =
   TriangleAttribs { corner1 :: !Attribs
                   , corner2 :: !Attribs
@@ -54,6 +58,8 @@ instance Storable TriangleAttribs where
     pokeElemOff ptr (off+1) corner2
     pokeElemOff ptr (off+2) corner3 
 
+-- Attributes for a quad. Implemented as two triangles with some
+-- redundancy.
 data QuadAttribs =
   QuadAttribs { upperLeft  :: !TriangleAttribs
               , lowerRight :: !TriangleAttribs
@@ -70,9 +76,8 @@ instance Storable QuadAttribs where
     pokeElemOff ptr  off    upperLeft
     pokeElemOff ptr (off+1) lowerRight
 
-pokeSpriteOff :: Ptr QuadAttribs -> Int -> Sprite -> AffineTransform GLfloat -> IO ()
-pokeSpriteOff ptr off Sprite{..} t =
-  pokeElemOff ptr off $
+spriteAttribs :: Sprite -> AffineTransform GLfloat -> QuadAttribs
+spriteAttribs Sprite{..} t =
   QuadAttribs
   (TriangleAttribs
    (Attribs ulx uly spriteLeft  spriteTop)
@@ -85,12 +90,61 @@ pokeSpriteOff ptr off Sprite{..} t =
   where (V2 llx lly, V2 ulx uly, V2 lrx lry, V2 urx ury) = applyFourCorners01 t
 
 bufferBytes :: Num a => a
-bufferBytes = 8*1024*1024
+bufferBytes = 4*1024*1024
 
--- TODO this is a pretty ugly function
-chunksToDraw :: [(Sprite, AffineTransform GLfloat)] -> [[(Int, Sprite, AffineTransform GLfloat)]]
+bufferLen :: Int
+bufferLen = bufferBytes `div` sizeOf (undefined :: QuadAttribs)
+
+-- Given a list of sprites to draw, divide it into chunks, each of
+-- which corresponds to a draw call. Every time we switch textures,
+-- that's a draw call. Every time we reach the end of the buffer,
+-- that's a draw call and a buffer orphaning.
+chunksToDraw :: [(Sprite, AffineTransform GLfloat)] ->
+                [[(Int, Sprite, AffineTransform GLfloat)]]
 chunksToDraw =
   groupBy (\(_,x,_) (j,y,_) -> spriteTexId x == spriteTexId y && j /= 0) .
-  zipWith (\i (s, t) -> (i, s, t))
-  (cycle [0..(bufferBytes `div` sizeOf (undefined :: QuadAttribs))])
+  zipWith (\i (s, t) -> (i, s, t)) (cycle [0..bufferLen])
 
+type VBO = GLuint
+
+drawChunks :: VBO -> [[(Int, Sprite, AffineTransform GLfloat)]] -> IO ()
+drawChunks vbo chunks = do
+  -- use vao (assumed to already be set up)
+  -- set current program
+  -- bind vbo (assumed to already be allocated)
+  -- check/set framebuffer?
+  -- stream and draw!
+  -- restore state
+  undefined
+
+-- TODO indexed draws?
+
+-- | Draw the given chunk. Returns false if the draw call was skipped
+-- due to a buffer mapping error. This should be rare and temporary,
+-- so instead of trying to handle it we just report it.
+drawChunk :: GLint -> [(Int, Sprite, AffineTransform GLfloat)] -> IO Bool
+drawChunk _ [] = error "empty chunk"
+drawChunk texUniform chunk@((offset, spr, _):_) = do
+  let offsetBytes = fromIntegral $ offset * sizeOf (undefined :: QuadAttribs)
+      rangeBytes = (bufferLen - offset) * sizeOf (undefined :: QuadAttribs)
+      invalidateBufferBit
+        | offset == 0 = gl_MAP_INVALIDATE_BUFFER_BIT
+        | otherwise   = 0
+      attribs = map (\(_, s, t) -> spriteAttribs s t) chunk
+  glBindTexture gl_TEXTURE_2D $ spriteTexId spr
+  glUniform1i texUniform 0
+  glActiveTexture gl_TEXTURE0
+  ptr <- glMapBufferRange gl_ARRAY_BUFFER offsetBytes (fromIntegral rangeBytes) $
+         gl_MAP_WRITE_BIT            .|.
+         gl_MAP_INVALIDATE_RANGE_BIT .|.
+         gl_MAP_FLUSH_EXPLICIT_BIT   .|.
+         gl_MAP_UNSYNCHRONIZED_BIT   .|.
+         invalidateBufferBit
+  flushCount <- foldM (\off a -> off + 1 <$ pokeElemOff ptr off a) 0 attribs
+  glFlushMappedBufferRange gl_ARRAY_BUFFER 0 . fromIntegral $
+    flushCount * sizeOf (undefined :: Attribs)
+  unmapSuccess <- glUnmapBuffer gl_ARRAY_BUFFER
+  let shouldDraw = unmapSuccess == fromIntegral gl_TRUE
+  when shouldDraw . glDrawArrays gl_TRIANGLES (fromIntegral offset) $
+    fromIntegral flushCount
+  return shouldDraw
