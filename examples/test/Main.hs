e5d1110e25bf0416ab53cc997d6f8d1e2bc3d3d3
Jake McArthur <Jake.McArthur@gmail.com>
2013-06-13 23:11:19 -0400

    make the example less computationally expensive and a little more interesting looking

--- examples/test/Main.hs
+++ examples/test/Main.hs
@@ -2,7 +2,6 @@ import Control.Applicative
 import Control.Monad
 import Data.Colour
 import Data.Colour.Names
-import Data.Time
 import Game.Graphics
 import Graphics.UI.GLFW (DisplayOptions (..))
 import Linear.V2
@@ -30,20 +29,17 @@ main = do
   unless glfwInitialized $ error "failed to initialize GLFW"
   windowOpened <- GLFW.openWindow displayOptions
   unless windowOpened $ error "failed to open window"
-  GLFW.setWindowTitle "Foo"
-  GLFW.setWindowBufferSwapInterval 0
+  GLFW.setWindowTitle "Wizard!"
   graphicsState <- initializeGraphics
-  tex <- either error id <$> loadTexture Nearest "examples/test/wizard/wizard.png"
+  tex <- either error id <$> loadTexture Linear "examples/test/wizard/wizard.png"
   let spr1 = mapTransform fromIntegral $ sprite (V2 3 7) (V2 55 82) tex
       spr2 = mapTransform fromIntegral $ modulatedSprite ((yellowgreen :: Colour Double) `withOpacity` 0.75) (V2 2 100) (V2 52 80) tex
-      image = do
+      image x = do
         scale $ V2 (recip $ windowWidth / 2) (recip $ windowHeight / 2)
-        msum . take 20000 $ cycle [spr1, translate (V2 (-20) 0) *> spr2]
-  start <- getCurrentTime
-  forM_ [0..999::Int] $ \_ -> do
+        let scaleOf1 = x / 500 + 0.25
+        scale (V2 scaleOf1 scaleOf1) *> spr1 <|> translate (V2 (500-x) 0) *> rotate (x*pi/500) *> scale 3 *> spr2
+  forM_ [0..999] $ \x -> do
     clear
-    _ <- draw graphicsState image
+    _ <- draw graphicsState $ image x
     GLFW.swapBuffers
-  stop <- getCurrentTime
-  print $ stop `diffUTCTime` start / 1000
   GLFW.terminate
