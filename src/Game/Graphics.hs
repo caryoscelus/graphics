9c21d17b92f1ab141a64010ffc2b90452c4d1c45
Jake McArthur <Jake.McArthur@gmail.com>
2013-06-15 13:08:30 -0400

    put Foldable and Traversable in for the new representation of Space

--- src/Game/Graphics.hs
+++ src/Game/Graphics.hs
@@ -13,12 +13,15 @@ module Game.Graphics
        ) where
 
 import Control.Applicative
+import Control.Arrow
 import Control.Monad
 import Control.Monad.Fix
+import Control.Monad.Trans.Writer.Strict
 import Control.Monad.Trans.State.Strict
 import Data.Colour
 import Data.Colour.Names (white)
 import Data.Foldable
+import Data.Traversable
 import Data.Monoid
 import Data.Word
 import Game.Graphics.AffineTransform (AffineTransform)
@@ -31,19 +34,22 @@ import qualified Game.Graphics.AffineTransform as Transform
 import qualified Game.Graphics.Sprite          as Sprite
 import qualified Game.Graphics.Stream          as Stream
 
--- TODO (<|>) stacks the right argument on top of the left argument. I
--- think I want it to be the other way around.
-
--- TODO Foldable and Traversable (yes, it can be done, since this is
--- actually a WriterT in disguise!)
-
 newtype Space c a = Space { unSpace :: StateT (AffineTransform c) [] a }
-                  deriving ( Functor, {- Foldable, Traversable, -} Applicative
-                           , Alternative, Monad, MonadPlus, MonadFix
+                  deriving ( Functor, Applicative , Alternative, Monad
+                           , MonadPlus, MonadFix
                            )
 
+instance Num c => Foldable (Space c) where
+  foldMap f = foldMap (f . fst) . runSpace
+
+instance Num c => Traversable (Space c) where
+  traverse f = fmap (space . runWriterT) . traverse f . WriterT . runSpace
+
+space :: Num c => [(a, AffineTransform c)] -> Space c a
+space xs = Space . StateT $ \a -> (map.second) (a <>) xs
+
 runSpace :: Num c => Space c a -> [(a, AffineTransform c)]
-runSpace = toList . (`runStateT` mempty) . unSpace
+runSpace = (`runStateT` mempty) . unSpace
 
 -- TODO put these into a MonadSpace class?
 
@@ -82,6 +88,5 @@ sprite :: V2 Word -> V2 Word -> Texture -> Space Int Sprite
 sprite = modulatedSprite (opaque (white :: Colour GLfloat))
 
 mapTransform :: (Num t, Num u) => (t -> u) -> Space t a -> Space u a
-mapTransform f =
-  Space . StateT . (\xs a -> map (\(x, a') -> (x, a <> fmap f a')) xs) .
-  (`runStateT` mempty) . unSpace
+mapTransform f = space . (map.second.fmap) f . runSpace
+  
