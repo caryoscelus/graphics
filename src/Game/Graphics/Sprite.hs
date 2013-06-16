e2a571272d14f24a57e6101e420a9772fcb007d7
Jake McArthur <Jake.McArthur@gmail.com>
2013-06-16 12:12:40 -0400

    throw an exception if you attempt to load a texture that exceeds driver limits

--- src/Game/Graphics/Sprite.hs
+++ src/Game/Graphics/Sprite.hs
@@ -1,8 +1,9 @@
 {-# OPTIONS -funbox-strict-fields #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ViewPatterns #-}
 module Game.Graphics.Sprite
-       ( Texture (), Sprite (..), Sampling (..)
+       ( Texture (), Sprite (..), Sampling (..), TextureError (..)
        , loadTexture, loadTexturePremultiplied
        , texture, texturePremultiplied
        , modulatedSprite
@@ -13,10 +14,12 @@ module Game.Graphics.Sprite
 import Codec.Picture
 import Codec.Picture.Types
 import Control.Applicative
+import Control.Exception
 import Control.Monad
 import Data.Colour
 import Data.Colour.SRGB
 import Data.Colour.SRGB.Linear
+import Data.Typeable
 import Data.Word
 import Foreign.Ptr
 import Foreign.Storable
@@ -75,13 +78,18 @@ setSampling Linear = do
   glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MIN_FILTER $ fromIntegral gl_LINEAR_MIPMAP_LINEAR
   glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MAG_FILTER $ fromIntegral gl_LINEAR
 
+data TextureError =
+  DimensionsTooLarge { driverLimit  :: !Word
+                     , actualWidth  :: !Word
+                     , actualHeight :: !Word
+                     } deriving (Show, Typeable)
+
+instance Exception TextureError
+
 -- | Create a texture from an image loaded using JuicyPixels.
 texture' :: (Image PixelRGBA8 -> Image PixelRGBA8) -> Sampling -> DynamicImage -> IO Texture
-texture' preprocess sampling dynImg = do
-  origTid <- glGet gl_TEXTURE_BINDING_2D
-  tid <- glGen glGenTextures
-  glBindTexture gl_TEXTURE_2D tid
-  (w, h) <- case dynImg of
+texture' preprocess sampling dynImg =
+  case dynImg of
     ImageY8     img -> texImage2D gl_SRGB8        gl_RED  gl_UNSIGNED_BYTE img
     ImageYF     img -> texImage2D gl_RGB32F       gl_RED  gl_FLOAT         img
     ImageYA8    img -> texImage2D gl_SRGB8_ALPHA8 gl_RGBA gl_UNSIGNED_BYTE $ preprocess (promoteImage img :: Image PixelRGBA8)
@@ -89,18 +97,22 @@ texture' preprocess sampling dynImg = do
     ImageRGBF   img -> texImage2D gl_RGB32F       gl_RGB  gl_FLOAT         img
     ImageRGBA8  img -> texImage2D gl_SRGB8_ALPHA8 gl_RGBA gl_UNSIGNED_BYTE $ preprocess img
     ImageYCbCr8 img -> texImage2D gl_SRGB8        gl_RGB  gl_UNSIGNED_BYTE (convertImage img :: Image PixelRGB8)
-  setSampling sampling
-  glTexParameteri gl_TEXTURE_2D gl_TEXTURE_WRAP_S $ fromIntegral gl_CLAMP_TO_EDGE
-  glTexParameteri gl_TEXTURE_2D gl_TEXTURE_WRAP_T $ fromIntegral gl_CLAMP_TO_EDGE
-  glBindTexture gl_TEXTURE_2D origTid
-  return $! Texture tid $ V2 w h
-  where texImage2D :: Storable (PixelBaseComponent b) => GLenum -> GLenum -> GLenum -> Image b -> IO (Word, Word)
+  where texImage2D :: Storable (PixelBaseComponent b) => GLenum -> GLenum -> GLenum -> Image b -> IO Texture
         texImage2D internal format type_ img = do
-          let w = imageWidth img
-              h = imageHeight img
-          unsafeWith img $ glTexImage2D gl_TEXTURE_2D 0 (fromIntegral internal)
-            (fromIntegral w) (fromIntegral h) 0 format type_
-          return $! (fromIntegral w, fromIntegral h)
+          let w, h :: Num a => a
+              w = fromIntegral $ imageWidth img
+              h = fromIntegral $ imageHeight img :: Num a => a
+          maxDim <- glGet gl_MAX_TEXTURE_SIZE
+          when (maxDim < w || maxDim < h) . throwIO $ DimensionsTooLarge maxDim w h
+          origTid <- glGet gl_TEXTURE_BINDING_2D
+          tid <- glGen glGenTextures
+          glBindTexture gl_TEXTURE_2D tid
+          unsafeWith img $ glTexImage2D gl_TEXTURE_2D 0 (fromIntegral internal) w h 0 format type_
+          setSampling sampling
+          glTexParameteri gl_TEXTURE_2D gl_TEXTURE_WRAP_S $ fromIntegral gl_CLAMP_TO_EDGE
+          glTexParameteri gl_TEXTURE_2D gl_TEXTURE_WRAP_T $ fromIntegral gl_CLAMP_TO_EDGE
+          glBindTexture gl_TEXTURE_2D origTid
+          return $! Texture tid $ V2 w h
         unsafeWith :: Storable (PixelBaseComponent a) => Image a -> (Ptr (PixelBaseComponent a) -> IO b) -> IO b
         unsafeWith = Vector.unsafeWith . imageData
 
