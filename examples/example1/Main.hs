b498cc1ac24f2fcdc2c586aa206e5dae7ca9a435
Jake McArthur <Jake.McArthur@gmail.com>
2013-07-11 21:53:32 -0400

    use the new GLFW-b for the example

--- examples/example1/Main.hs
+++ examples/example1/Main.hs
@@ -2,9 +2,9 @@ import           Control.Applicative
 import           Control.Monad
 import           Data.Colour
 import           Data.Colour.Names
+import           Data.Maybe
 import           Data.Time
 import           Game.Graphics
-import           Graphics.UI.GLFW    (DisplayOptions (..))
 import qualified Graphics.UI.GLFW    as GLFW
 import           Linear.V2
 
@@ -12,25 +12,23 @@ windowWidth, windowHeight :: Num a => a
 windowWidth  = 1024
 windowHeight = 768
 
-displayOptions :: DisplayOptions
-displayOptions =
-  GLFW.defaultDisplayOptions { displayOptions_width                   = windowWidth
-                             , displayOptions_height                  = windowHeight
-                             , displayOptions_numRedBits              = 8
-                             , displayOptions_numGreenBits            = 8
-                             , displayOptions_numBlueBits             = 8
-                             , displayOptions_windowIsResizable       = False
-                             , displayOptions_openGLVersion           = (2,1)
-                             }
-
 main :: IO ()
 main = do
-  glfwInitialized <- GLFW.initialize
+  GLFW.setErrorCallback . Just $ \e str -> putStrLn $ show e ++ ": " ++ str
+  glfwInitialized <- GLFW.init
   unless glfwInitialized $ error "failed to initialize GLFW"
-  windowOpened <- GLFW.openWindow displayOptions
-  unless windowOpened $ error "failed to open window"
-  GLFW.setWindowTitle "Wizard!"
-  GLFW.setWindowBufferSwapInterval 0
+  mapM_ GLFW.windowHint [ GLFW.WindowHint'Resizable False
+                        , GLFW.WindowHint'AlphaBits 0
+                        , GLFW.WindowHint'DepthBits 0
+                        , GLFW.WindowHint'StencilBits 0
+                        , GLFW.WindowHint'sRGBCapable True
+                        , GLFW.WindowHint'ContextVersionMajor 2
+                        , GLFW.WindowHint'ContextVersionMinor 1
+                        ]
+  window <- fromMaybe (error "failed to create window") <$>
+            GLFW.createWindow windowWidth windowHeight "Wizard!" Nothing Nothing
+  GLFW.makeContextCurrent $ Just window
+  GLFW.swapInterval 0
   glViewport 0 0 windowWidth windowHeight
   graphicsState <- initializeGraphics
   tex <- either error id <$> loadTexture Standard Linear "examples/example1/wizard/wizard.png"
@@ -47,10 +45,12 @@ main = do
   forM_ [1..frames] $ \x -> do
     clear
     _ <- draw graphicsState $ image x
-    GLFW.swapBuffers
+    return ()
+    GLFW.swapBuffers window
   stop <- getCurrentTime
   let spf = realToFrac $ stop `diffUTCTime` start / frames :: Double
   putStrLn $ show (round $ spf * 1000000 :: Int) ++ " us/frame"
   freeTexture tex
   freeGraphics graphicsState
+  GLFW.destroyWindow window
   GLFW.terminate
